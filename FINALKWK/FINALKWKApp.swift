114352db493cded21c3dc833d14912f49634c2e8
HalleWooding <halle@wooding5.com>
2023-06-26 10:48:53 -0700

    Initial Commit

---
+++ FINALKWK/FINALKWKApp.swift
@@ -0,0 +1,17 @@
+//
+//  FINALKWKApp.swift
+//  FINALKWK
+//
+//  Created by Halle Wooding on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct FINALKWKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
