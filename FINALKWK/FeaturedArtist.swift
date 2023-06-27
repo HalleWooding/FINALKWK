ca8f0655e11668aa52cb6889f3d07f68cc714cac
Jenny Trexler <jennytrexler@gmail.com>
2023-06-27 16:29:15 -0700

    added colors

--- FINALKWK/FeaturedArtist.swift
+++ FINALKWK/FeaturedArtist.swift
@@ -9,12 +9,14 @@ import SwiftUI
 
 struct FeaturedArtist: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image("")
+        }
     }
-}
-
-struct FeaturedArtist_Previews: PreviewProvider {
-    static var previews: some View {
-        FeaturedArtist()
+    
+    struct FeaturedArtist_Previews: PreviewProvider {
+        static var previews: some View {
+            FeaturedArtist()
+        }
     }
 }
