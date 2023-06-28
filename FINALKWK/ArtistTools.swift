2c283495570fac328521dc974d3d38edfc665e5f
HalleWooding <halle@wooding5.com>
2023-06-28 14:04:14 -0700

    changes

--- FINALKWK/ArtistTools.swift
+++ FINALKWK/ArtistTools.swift
@@ -10,75 +10,83 @@ import SwiftUI
 
 struct ArtistTools: View {
     var body: some View {
+        
+        
         NavigationStack {
-            VStack {
-                HStack {
-                    NavigationLink(destination: Inspo()) {
-                        Text("Artist's Inspiration")
-                            .foregroundColor (.white)
-                            .padding()
-                            .background (Color("LightPurple"))
-                            .fontWeight(.bold)
-                            .font(.system(.title3, design: .serif))
-                            .cornerRadius (10)
+            ZStack {
+                Color ("Pink")
+                    .ignoresSafeArea()
+                VStack {
+                    
+                    HStack {
+                        NavigationLink(destination: Inspo()) {
+                            Text("Artist's Inspiration")
+                                .foregroundColor (.white)
+                                .padding()
+                                .background (Color("LightPurple"))
+                                .fontWeight(.bold)
+                                .font(.system(.title3, design: .serif))
+                                .cornerRadius (10)
                             
-                    }
-                    
-                    .padding()
-                    Text("Learn where this artist found inspiration!")
+                        }
+                        
                         .padding()
-                }
-                HStack {
-                    NavigationLink(destination: Materials()) {
-                        Text("Artist's Materials")
-                            .foregroundColor (.white)
+                        Text("Learn where this artist found inspiration!")
                             .padding()
-                            .background (Color("LightPurple"))
-                            .fontWeight(.bold)
-                            .font(.system(.title3, design: .serif))
-                            .cornerRadius (10)
                     }
-                    
-                    .padding()
-                    Text ("Learn what materials this artist gravitates towards!")
+                    HStack {
+                        NavigationLink(destination: Materials()) {
+                            Text("Artist's Materials")
+                                .foregroundColor (.white)
+                                .padding()
+                                .background (Color("LightPurple"))
+                                .fontWeight(.bold)
+                                .font(.system(.title3, design: .serif))
+                                .cornerRadius (10)
+                        }
+                        
                         .padding()
-                }
-
-                HStack {
-                    NavigationLink(destination: Palette()) {
-                        Text("Artist's Palettes")
-                            .foregroundColor (.white)
+                        Text ("Learn what materials this artist gravitates towards!")
                             .padding()
-                            .background (Color("LightPurple"))
-                            .fontWeight(.bold)
-                            .font(.system(.title3, design: .serif))
-                            .cornerRadius (10)
                     }
                     
-                    .padding()
-                    Text ("A palette/palettes of some of the artist's art!")
+                    HStack {
+                        NavigationLink(destination: Palette()) {
+                            Text("Artist's Palettes")
+                                .foregroundColor (.white)
+                                .padding()
+                                .background (Color("LightPurple"))
+                                .fontWeight(.bold)
+                                .font(.system(.title3, design: .serif))
+                                .cornerRadius (10)
+                        }
+                        
                         .padding()
-                }
-                HStack {
-                    NavigationLink(destination: Tips()) {
-                        Text("Artist's Tips")
-                            .foregroundColor (.white)
+                        Text ("A palette/palettes of some of the artist's art!")
                             .padding()
-                            .background (Color("LightPurple"))
-                            .fontWeight(.bold)
-                            .font(.system(.title3, design: .serif))
-                            .cornerRadius (10)
+                    }
+                    HStack {
+                        NavigationLink(destination: Tips()) {
+                            Text("Artist's Tips")
+                                .foregroundColor (.white)
+                                .padding()
+                                .background (Color("LightPurple"))
+                                .fontWeight(.bold)
+                                .font(.system(.title3, design: .serif))
+                                .cornerRadius (10)
+                        }
+                        
+                        .padding()
+                        Text ("What niche knowledge can you learn from this artist?")
+                            .padding()
                     }
                     
-                    .padding()
-                    Text ("What niche knowledge can you learn from this artist?")
-                        .padding()
                 }
-                
             }
         }
+        }
     }
-}
+
 
 struct ArtistTools_Previews: PreviewProvider {
     static var previews: some View {
