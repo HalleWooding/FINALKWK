8d311bdb714727f30639b981d366c786f752a42d
chrissitina5 <111199410+chrissitina5@users.noreply.github.com>
2023-06-28 14:07:21 -0700

    changes to homepage

--- FINALKWK/HomeScreen.swift
+++ FINALKWK/HomeScreen.swift
@@ -9,19 +9,43 @@ import SwiftUI
 
 struct HomeScreen: View {
     var body: some View {
-        VStack {
-            Image("temporary")
-                .resizable(resizingMode: .stretch)
-                .aspectRatio(contentMode: .fit)
-                .padding(.all)
-            Text("[artist's name]")
+        VStack(spacing: 20.0) {
+            ZStack {
+                Color("Pink")
+                    .ignoresSafeArea()
+                VStack(spacing: 20.0) {
+                    Image("temporary")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                        .padding(.all)
+                    Text("[artist's name]")
+                    Text("[artist's quote]")
+                } .padding()
+                    .background(Rectangle() .foregroundColor(.white))
+                    .cornerRadius(15)
+                    .shadow(radius: 15)
+                    .padding()
+            }
+            ZStack {
+                Color("Pink")
+                    .ignoresSafeArea()
+                NavigationStack {
+                    HStack {
+                        NavigationLink(destination: Calendar()) {
+                            Image(systemName: "calendar.badge.clock")
+                        NavigationLink (destination: FeaturedArtist()) {
+                            Image(systemName: "paintbrush")
+                            }
+                        }
+                    }
+                }
+            }
         }
-        
     }
-}
-
-struct HomeScreen_Previews: PreviewProvider {
-    static var previews: some View {
-        HomeScreen()
+    
+    struct HomeScreen_Previews: PreviewProvider {
+        static var previews: some View {
+            HomeScreen()
+        }
     }
 }
