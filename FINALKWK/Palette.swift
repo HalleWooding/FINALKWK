f3d43d1c6901fb2e461d8bd2006ac92624727e79
HalleWooding <halle@wooding5.com>
2023-06-28 13:19:44 -0700

    commit

--- FINALKWK/Palette.swift
+++ FINALKWK/Palette.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct Palette: View {
     var body: some View {
         Text("Artist's Main Palettes")
-}
-
-struct Palette_Previews: PreviewProvider {
-    static var previews: some View {
-        Palette()
+    }
+    
+    struct Palette_Previews: PreviewProvider {
+        static var previews: some View {
+            Palette()
+        }
     }
 }
