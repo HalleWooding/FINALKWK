0592f3e4214a95db095a82737af485700aec5ce8
chrissitina5 <111199410+chrissitina5@users.noreply.github.com>
2023-06-29 12:04:33 -0700

    Finished

--- FINALKWK/Calendar.swift
+++ FINALKWK/Calendar.swift
@@ -16,12 +16,18 @@ struct Calendar: View {
     
 //  TEXT VARIABLES
     var backgroundColorCalendar = "DarkPurple"
+    var textColorCalendar = Color(red: 240, green: 240, blue: 240)
+    
+// OTHER
+    var imageRadius = CGFloat(40)
+    var imageDimensions = CGFloat(80)
+    var worksOfArt = ["SonyaClark-photo", "RachelRuysch", "HungLiu", "MwangiHutter", "horseman", "mona-lisa", "starry-night"]
     
     
 //  MONTH OF YEAR FUNCTION
     var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
-    var firstMonth = 0
+    var firstMonth = 2
 
     func countingMonths(monthPosition : Int) -> String{
         let month = months[firstMonth + monthPosition]
@@ -34,46 +40,103 @@ struct Calendar: View {
     var body: some View {
         NavigationStack{
             ZStack{
-//                Color(backgroundColorCalendar)
-//                    .ignoresSafeArea()
+                Color(backgroundColorCalendar)
+                    .ignoresSafeArea()
+                
                 VStack{
     // MONTH 1
                     HStack{
-                        Text(countingMonths(monthPosition : 0))
+                        Text(countingMonths(monthPosition : 3))
                             .padding(.leading, titlePadding)
                             .padding(.top, titleTop)
+                            .foregroundStyle(textColorCalendar)
                         Spacer()
                     }
                     
                     HStack{
-                        Circle()
+                        ZStack{
+                            Circle()
+                            NavigationLink(destination: FeaturedArtist()){
+                                Image(worksOfArt[0])
+                                    .resizable(resizingMode: .stretch)
+                                                            .frame(width: imageDimensions, height: imageDimensions)
+                                                        .aspectRatio(contentMode: .fit)
+                                                        .aspectRatio(contentMode: .fit)
+                                                        .cornerRadius(imageRadius)
+                            }
+                        }
+                        
+                        ZStack{
+                            Circle()
+                            Image(worksOfArt[1])
+                                .resizable(resizingMode: .stretch)
+                                                        .frame(width: imageDimensions, height: imageDimensions)
+                                                    .aspectRatio(contentMode: .fit)
+                                                    .aspectRatio(contentMode: .fit)
+                                                    .cornerRadius(imageRadius)
+
+                        }
+                        
+                        ZStack{
+                            Circle()
+                            Image(worksOfArt[2])
+                                .resizable(resizingMode: .stretch)
+                                                        .frame(width: imageDimensions, height: imageDimensions)
+                                                    .aspectRatio(contentMode: .fit)
+                                                    .aspectRatio(contentMode: .fit)
+                                                    .cornerRadius(imageRadius)
+
+                        }
+                        
+                        ZStack{
+                            Circle()
+//                                .shadow(radius: 10)
+                            Image(worksOfArt[3])
+                                .resizable(resizingMode: .stretch)
+                                                        .frame(width: imageDimensions, height: imageDimensions)
+                                                    .aspectRatio(contentMode: .fit)
+                                                    .aspectRatio(contentMode: .fit)
+                                                    .cornerRadius(imageRadius)
+
+                        }
+                        
+                        
     //                        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-                        Circle()
-                        Circle()
-                        Circle()
                     } .padding(.leading, calendarPadding)
                     .padding(.trailing, calendarPadding)
     // MONTH 2
                     HStack{
-                        Text(countingMonths(monthPosition : 1))
+                        Text(countingMonths(monthPosition : 2))
                             .padding(.leading, titlePadding)
                             .padding(.top, titleTop)
+                            .foregroundStyle(textColorCalendar)
                         Spacer()
                     }
 
                     HStack{
                         Circle()
     //                        .shadow(radius: 10)
-                        Circle()
+                        ZStack{
+                            Circle()
+//                                .shadow(radius: 10)
+                            Image(worksOfArt[4])
+                                .resizable(resizingMode: .stretch)
+                                                        .frame(width: 50, height: 50)
+                                                    .aspectRatio(contentMode: .fit)
+                                                    .aspectRatio(contentMode: .fit)
+//                                                    .cornerRadius(imageRadius)
+
+                        }
                         Circle()
                         Circle()
                     } .padding(.leading, calendarPadding)
                     .padding(.trailing, calendarPadding)
     // MONTH 3
                     HStack{
-                        Text(countingMonths(monthPosition : 2))
+                        Text(countingMonths(monthPosition : 1))
                             .padding(.leading, titlePadding)
                             .padding(.top, titleTop)
+                            .foregroundStyle(textColorCalendar)
                         Spacer()
                     }
 
@@ -81,23 +144,41 @@ struct Calendar: View {
                         Circle()
     //                        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
                         Circle()
-                        Circle()
+                        ZStack{
+                            Circle()
+//                                .shadow(radius: 10)
+                            Image(worksOfArt[5])
+                                .resizable(resizingMode: .stretch)
+                                                        .frame(width: 50, height: 50)
+                                                    .aspectRatio(contentMode: .fit)
+                                                    .aspectRatio(contentMode: .fit)
+//                                                    .cornerRadius(imageRadius)
+
+                        }
                         Circle()
                     } .padding(.leading, calendarPadding)
                     .padding(.trailing, calendarPadding)
                     
     // MONTH 4
                     HStack{
-                        Text(countingMonths(monthPosition : 3))
+                        Text(countingMonths(monthPosition : 0))
                             .padding(.leading, titlePadding)
                             .padding(.top, titleTop)
+                            .foregroundStyle(textColorCalendar)
                         Spacer()
                     }
 
                     HStack{
                         ZStack{
                             Circle()
-                            Image("star-color")
+//                                .shadow(radius: 10)
+                            Image(worksOfArt[6])
+                                .resizable(resizingMode: .stretch)
+                                                        .frame(width: 50, height: 50)
+                                                    .aspectRatio(contentMode: .fit)
+                                                    .aspectRatio(contentMode: .fit)
+//                                                    .cornerRadius(imageRadius)
+
                         }
 //                        Circle()
     //                        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
@@ -110,33 +191,31 @@ struct Calendar: View {
                     .toolbar {
                     ToolbarItemGroup(placement: .bottomBar) {
                         ZStack {
-                                
                         HStack {
                             NavigationLink(destination: HomeScreen()) {
                                 Image(systemName: "house")
-                                    .foregroundColor(Color("Yellow"))
-                            }.padding(.all)
-                                .navigationBarBackButtonHidden(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                                    .foregroundColor(Color("DarkPurple"))
+                            }.padding(.all).navigationBarBackButtonHidden(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
                             NavigationLink(destination: FeaturedArtist()) {
                                 Image(systemName: "sparkles")
-                                    .foregroundColor(Color("Yellow"))
+                                    .foregroundColor(Color("DarkPurple"))
                             }.padding(.all)
-                                .navigationBarBackButtonHidden(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                            .navigationBarBackButtonHidden(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
                             NavigationLink(destination: ArtistTools()) {
                                 Image(systemName: "wrench.and.screwdriver")
-                                    .foregroundColor(Color("Yellow"))
+                                    .foregroundColor(Color("DarkPurple"))
                             }.padding(.all)
                                 .navigationBarBackButtonHidden(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
                         }
-                            .background(Rectangle() .foregroundColor(Color("DarkPurple")))
+                            .background(Rectangle() .foregroundColor(Color("Yellow")))
                             .cornerRadius(15)
                             .shadow(radius: 15)
                             }
                         }
-                    }
+                    }// Toolbar
                     
                 } // VStack
-            } //ZStack
+            } // ZStack
         } // NavStack
         
     }
