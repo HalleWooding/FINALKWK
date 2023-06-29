c6b310cd16b29b06f5e12606ea4f9409f6617408
Jenny Trexler <jennytrexler@gmail.com>
2023-06-29 11:41:10 -0700

    new log in and sign up, added images

---
+++ FINALKWK/LogIn.swift
@@ -0,0 +1,20 @@
+//
+//  LogIn.swift
+//  FINALKWK
+//
+//  Created by Jenny Trexler on 6/29/23.
+//
+
+import SwiftUI
+
+struct LogIn: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LogIn_Previews: PreviewProvider {
+    static var previews: some View {
+        LogIn()
+    }
+}
